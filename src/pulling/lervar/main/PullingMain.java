077252fed09ad3ad93e57185eb9184f5e5f440e1
J_pc <1691365315@qq.com>
2026-02-02 20:44:58 +0800

    _

--- src/pulling/lervar/main/PullingMain.java
+++ src/pulling/lervar/main/PullingMain.java
@@ -1,4 +1,17 @@
+/**
+ * LerVar_LanguagePulling(v.pre-?.?_Beta?, v.release-?.?.?)
+ * develop:
+ * -with: [LerVar]
+ * -for: [LerVar]
+ * <p>
+ * @since 2026
+ * Copyright (c) 2026
+ * J_pc and/or his studios
+ * All rights reserved
+ */
+
 package pulling.lervar.main;
 
 public class PullingMain {
+
 }
---
+++ src/pulling/lervar/output/TextPrint.java
@@ -0,0 +1,23 @@
+/**
+ * LerVar_LanguagePulling(v.pre-?.?_Beta?, v.release-?.?.?)
+ * develop:
+ * -with: [LerVar]
+ * -for: [LerVar]
+ * <p>
+ * @since 2026
+ * Copyright (c) 2026
+ * J_pc and/or his studios
+ * All rights reserved
+ */
+
+package pulling.lervar.output;
+
+import com.lervar.interfaces.of_lervar_output.of_languages_output.OptionPrintLanguages;
+import com.lervar.main.Type;
+
+public interface TextPrint extends OptionPrintLanguages {
+    @Override
+    default String[] optionPrintLanguages(Type t){
+        return new String[]{""};
+    }
+}
