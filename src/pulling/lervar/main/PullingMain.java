6d093220b6910fcdae599620f5cab771cef9cf2d
J_pc <1691365315@qq.com>
2026-02-17 10:01:30 +0800

    _

--- src/pulling/lervar/main/PullingMain.java
+++ src/pulling/lervar/main/PullingMain.java
@@ -5,9 +5,9 @@
  * -for: [LerVar]
  * <p>
  * @since 2026
- * Copyright (c) 2026
- * J_pc and/or his studios
- * All rights reserved
+ * Copyright (c) 2026J_pc and/or his studios
+ * SPDX-License-Identifier: MIT
+ * URL: https://github.com/J-pc-123/LerVar_LanguagePulling/blob/main/LICENSE
  */
 
 package pulling.lervar.main;
